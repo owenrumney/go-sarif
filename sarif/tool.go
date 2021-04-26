97d25788a72bd3b46bd0ba28e157e1c12f05901c
Mikhail Sugakov <misha@stackrox.com>
2021-04-26 17:41:41 +0200

    Extend `Driver` and `Rule` with additional attributes for GitHub
    SARIF Validator's GitHub rules ask to add Version for Driver.
    Validator also suggests it is desirable to populate Rule.Name,
    however can ingests files without it.
    Rule's full description can be displayed in GitHub UI therefore added
    it.
    
    Note that I switched `ShortDescription` and `Help` to
    `MultiformatMessageString`-s because that's the format according to
    the recent available schema. E.g.
    https://github.com/oasis-tcs/sarif-spec/blob/master/Schemata/sarif-schema-2.1.0.json#L1859-L1862

--- sarif/tool.go
+++ sarif/tool.go
@@ -6,17 +6,26 @@ type Tool struct {
 
 type Driver struct {
 	Name           string  `json:"name"`
+	Version        string  `json:"version,omitempty"`
 	InformationURI string  `json:"informationUri"`
 	Rules          []*Rule `json:"rules,omitempty"`
 }
 
+// WithVersion specifies tool version, in whatever format it natively provides. Returns updated driver.
+func (driver *Driver) WithVersion(version string) *Driver {
+	driver.Version = version
+	return driver
+}
+
 // Rule specifies a Sarif Rule object
 type Rule struct {
-	ID               string            `json:"id"`
-	ShortDescription *TextBlock        `json:"shortDescription"`
-	HelpURI          string            `json:"helpUri,omitempty"`
-	Help             *TextBlock        `json:"help,omitempty"`
-	Properties       map[string]string `json:"properties,omitempty"`
+	ID               string                    `json:"id"`
+	Name             string                    `json:"name,omitempty"`
+	ShortDescription *MultiformatMessageString `json:"shortDescription"`
+	FullDescription  *MultiformatMessageString `json:"fullDescription,omitempty"`
+	HelpURI          string                    `json:"helpUri,omitempty"`
+	Help             *MultiformatMessageString `json:"help,omitempty"`
+	Properties       map[string]string         `json:"properties,omitempty"`
 }
 
 func (driver *Driver) getOrCreateRule(rule *Rule) uint {
@@ -35,11 +44,25 @@ func newRule(ruleID string) *Rule {
 	}
 }
 
-// WithDescription specifies a description for a rule and returns the updated rule
+// WithName specifies rule name that is understandable to an end user and returns the updated rule.
+func (rule *Rule) WithName(name string) *Rule {
+	rule.Name = name
+	return rule
+}
+
+// WithDescription specifies short description for a rule and returns the updated rule.
+// Short description should be a single sentence that is understandable when visible space is limited to a single line
+// of text.
 func (rule *Rule) WithDescription(description string) *Rule {
-	rule.ShortDescription = &TextBlock{
-		Text: description,
-	}
+	rule.ShortDescription = NewMultiformatMessageString(description)
+	return rule
+}
+
+// WithFullDescription specifies full description for a rule and returns the updated rule.
+// Full description should, as far as possible, provide details sufficient to enable resolution of any problem indicated
+// by the result.
+func (rule *Rule) WithFullDescription(description *MultiformatMessageString) *Rule {
+	rule.FullDescription = description
 	return rule
 }
 
@@ -51,9 +74,7 @@ func (rule *Rule) WithHelpURI(helpURI string) *Rule {
 
 // WithHelp specifies a help text  for a rule and returns the updated rule
 func (rule *Rule) WithHelp(helpText string) *Rule {
-	rule.Help = &TextBlock{
-		Text: helpText,
-	}
+	rule.Help = NewMultiformatMessageString(helpText)
 	return rule
 }
 
