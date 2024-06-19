005651642499b06e3907c3fc71fa756d9d96d843
Wenkai Li <wenkai.li@ing.com>
2024-06-19 13:19:12 +0200

    Support Tool.Extensions property

--- v2/sarif/tool.go
+++ v2/sarif/tool.go
@@ -2,9 +2,9 @@ package sarif
 
 // Tool ...
 type Tool struct {
-	Driver *ToolComponent `json:"driver"`
+	Driver     *ToolComponent   `json:"driver"`
+	Extensions []*ToolComponent `json:"extensions,omitempty"`
 	PropertyBag
-
 }
 
 // NewTool creates a new Tool and returns a pointer to it
@@ -19,4 +19,15 @@ func NewSimpleTool(driverName string) *Tool {
 	return &Tool{
 		Driver: NewDriver(driverName),
 	}
+}
+
+// WithExtensions sets the Extensions
+func (tool *Tool) WithExtensions(extensions []*ToolComponent) *Tool {
+	tool.Extensions = extensions
+	return tool
+}
+
+// AddExtension ...
+func (tool *Tool) AddExtension(extension *ToolComponent) {
+	tool.Extensions = append(tool.Extensions, extension)
 }
