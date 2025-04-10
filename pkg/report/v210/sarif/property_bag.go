d46b177a033883a7d4912c5f65c26119875d209f
Viktor Kartashov <viktor@scribesecurity.com>
2025-04-10 16:15:43 +0300

    moved PropertyBag Marshal funcs to the companion file

--- pkg/report/v210/sarif/property_bag.go
+++ pkg/report/v210/sarif/property_bag.go
@@ -1,7 +1,5 @@
 package sarif
 
-import "encoding/json"
-
 // PropertyBag - Key/value pairs that provide additional information about the object.
 type PropertyBag struct {
 	// AdditionalProperties - additional properties
@@ -35,37 +33,4 @@ func (t *PropertyBag) WithTags(tags []string) *PropertyBag {
 func (t *PropertyBag) AddTag(tag string) *PropertyBag {
 	t.Tags = append(t.Tags, tag)
 	return t
-}
-
-// MarshalJSON - custom JSON marshaller for PropertyBag
-func (p PropertyBag) MarshalJSON() ([]byte, error) {
-	// type Alias PropertyBag
-	aux := make(map[string]interface{})
-	for k, v := range p.Properties {
-		aux[k] = v
-	}
-	if len(p.Tags) > 0 {
-		aux["tags"] = p.Tags
-	}
-	return json.Marshal(aux)
-}
-
-// UnmarshalJSON - custom JSON unmarshaller for PropertyBag
-func (p *PropertyBag) UnmarshalJSON(data []byte) error {
-	// type Alias PropertyBag
-	aux := struct {
-		Tags []string `json:"tags,omitempty"`
-	}{}
-	if err := json.Unmarshal(data, &aux); err != nil {
-		return err
-	}
-	p.Tags = aux.Tags
-
-	var raw map[string]interface{}
-	if err := json.Unmarshal(data, &raw); err != nil {
-		return err
-	}
-	delete(raw, "tags")
-	p.Properties = raw
-	return nil
 }
